d4381417d0dbf42baed02355b49138739eb363bb
tokkimenkyo-rgb <tokkimenkyo@gmail.com>
2026-07-25 10:54:03 +0900

    Fix middleware __dirname error by excluding bufferutil/utf-8-validate

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), 'bufferutil', 'utf-8-validate'];
+    return config;
+  },
 };
 
 export default withNextIntl(nextConfig);
