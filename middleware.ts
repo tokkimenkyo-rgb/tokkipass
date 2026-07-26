0d93fe72d38fa00b0636fc9bb4750c3e52d8fede
tokkimenkyo-rgb <tokkimenkyo@gmail.com>
2026-07-26 16:07:34 +0900

    Revert proxy.ts back to middleware.ts (proxy forces nodejs runtime)

--- middleware.ts
+++ middleware.ts
@@ -31,7 +31,7 @@ function detectLocale(request: NextRequest): Locale {
   return DEFAULT_LOCALE;
 }
 
-export function proxy(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
   if (!request.cookies.get('tokkipass-locale')) {
